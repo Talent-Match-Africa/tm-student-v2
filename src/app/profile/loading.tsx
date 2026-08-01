bce62ef330beddd94d154abb1b6d29a54d1e7b97
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-01 17:59:29 +0200

    Implement student profile app/profile/loading.tsx

---
+++ src/app/profile/loading.tsx
@@ -0,0 +1,5 @@
+import { ManagementFormSkeleton } from "@/components/shared/ManagementFormSkeleton";
+
+export default function ProfileLoading() {
+  return <ManagementFormSkeleton stepCount={5} />;
+}
