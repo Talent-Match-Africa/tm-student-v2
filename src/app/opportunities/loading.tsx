d1fc1649eea854b9d247907e260bda995c4f6992
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-01 18:26:26 +0200

    Match student opportunities behavior in app/opportunities/loading.tsx

---
+++ src/app/opportunities/loading.tsx
@@ -0,0 +1,5 @@
+import { OpportunitiesPageSkeleton } from "@/components/opportunities/OpportunitiesPageSkeleton";
+
+export default function OpportunitiesLoading() {
+  return <OpportunitiesPageSkeleton />;
+}
