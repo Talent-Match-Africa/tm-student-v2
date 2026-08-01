d591d2b892c3139fe835b834a7b7c025adb8b52c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-01 19:54:15 +0200

    Remove the student email confirmation loading boundary

--- src/app/profile/email/confirm/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { StudentEmailChangeConfirmationSkeleton } from "@/components/profile/StudentEmailChangeConfirmationSkeleton";
-
-export default function StudentEmailConfirmationLoading() {
-  return <StudentEmailChangeConfirmationSkeleton />;
-}
